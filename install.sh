1edac9c6ead62d06e66a13636a0c76379775a992
Porawit Poboonma <ball6847@gmail.com>
2015-04-29 23:54:49 +0700

    add installer for bang

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="profile bashrc vimrc zshrc"    # list of files/folders to symlink in homedir
+files="profile bashrc vimrc zshrc bang"    # list of files/folders to symlink in homedir
 
 ##########
 
