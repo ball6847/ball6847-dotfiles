9b6173dec6e4cc2514f28537be060974e8fecc8b
Porawit Poboonma <ball6847@gmail.com>
2024-03-28 18:25:45 +0700

    feat: make k3d up down to also start and stop default cluster

--- bin/k3d-down.sh
+++ bin/k3d-down.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-k3d cluster delete -c ~/.dotfiles/k3d.yml
+# stop the k3d cluster if it is running
+k3d cluster stop default
+
+# if --delete flag is provided, delete the k3d cluster
+if [[ $1 == "--delete" ]]; then
+	k3d cluster delete -c ~/.dotfiles/k3d.yml
+fi
--- bin/k3d-up.sh
+++ bin/k3d-up.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 k3d cluster create -c ~/.dotfiles/k3d.yml --kubeconfig-update-default
+k3d cluster start default
