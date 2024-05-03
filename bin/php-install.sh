c0217a564c5d46edcccbd2b7023aa7c8482f57dd
Porawit Poboonma <ball6847@gmail.com>
2024-05-03 13:40:25 +0700

    feat: add php7.2-mbstring

--- bin/php-install.sh
+++ bin/php-install.sh
@@ -3,4 +3,4 @@
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt install php7.2-cli php7.2-dom php8.3-cli 
+sudo apt install php7.2-cli php7.2-dom php7.2-mbstring php8.3-cli 
