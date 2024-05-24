4440dd0d025298dc96dd2a31b28048d148a5364a
Porawit Poboonma <ball6847@gmail.com>
2024-05-25 00:03:52 +0700

    feat(nvim): extended config for intelephense

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").nn_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "intelephense" }
+local servers = { "html", "cssls"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,7 +15,8 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- deno
+
+-- deno (typescript)
 -- see https://docs.deno.com/runtime/manual/getting_started/setup_your_environment#vimneovim-via-plugins
 lspconfig.denols.setup {
   on_attach = on_attach,
@@ -36,5 +37,33 @@ lspconfig.tsserver.setup {
   capabilities = capabilities,
   -- root_dir = lspconfig.util.root_pattern("package.json"),
   -- single_file_support = false
+}
+
+-- intelephense (php)
+lspconfig.intelephense.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    intelephense = {
+      -- Add wordpress to the list of stubs
+      stubs = {
+        "apache", "bcmath", "bz2", "calendar", "com_dotnet", "Core", "ctype", "curl", "date",
+        "dba", "dom", "enchant", "exif", "FFI", "fileinfo", "filter", "fpm", "ftp", "gd", "gettext",
+        "gmp", "hash", "iconv", "imap", "intl", "json", "ldap", "libxml", "mbstring", "meta", "mysqli",
+        "oci8", "odbc", "openssl", "pcntl", "pcre", "PDO", "pdo_ibm", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "pgsql",
+        "Phar", "posix", "pspell", "readline", "Reflection", "session", "shmop", "SimpleXML", "snmp", "soap",
+        "sockets", "sodium", "SPL", "sqlite3", "standard", "superglobals", "sysvmsg", "sysvsem", "sysvshm", "tidy",
+        "tokenizer", "xml", "xmlreader", "xmlrpc", "xmlwriter", "xsl", "Zend OPcache", "zip", "zlib",
+        "wordpress", "phpunit",
+      },
+      diagnostics = {
+        enable = true,
+      },
+      format = {
+        enable = true
+      }
+    },
+  }
 }
 
