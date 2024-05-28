351ec27f76773ce7789e402490c9aa48b9e2d012
Porawit Poboonma <ball6847@gmail.com>
2024-05-28 15:16:55 +0700

    feat: simple setup noice

---
+++ config/nvim/lua/configs/noice.lua
@@ -0,0 +1,13 @@
+-- TODO: nvchad already has default handler for lsp signature and hover,
+-- so we don't have any chance to try out lsp signature and hover handler.
+-- but we will do some day
+require("noice").setup({
+  lsp = {
+    signature = {
+      enabled = false
+    },
+    hover = {
+      enabled = false
+    },
+  },
+});
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -4,7 +4,7 @@ return {
     event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
-       -- local conform = require("conform")
+      -- local conform = require("conform")
 
       -- conform.setup({
       --   formatters_by_ft = {
@@ -73,18 +73,18 @@ return {
     end,
   },
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server",
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
         "stylua",
-  			"html-lsp",
+        "html-lsp",
         "css-lsp" ,
         "prettier",
         "deno",
         "intelephense"
-  		},
-  	},
+      },
+    },
   },
   --
   -- {
@@ -96,4 +96,24 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  -- noice
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require "configs.noice"
+    end,
+  }
 }
