5d8d188751ab6644566ebd71f265138a3a5ee105
Porawit Poboonma <ball6847@gmail.com>
2024-05-28 17:49:39 +0700

    feat: configure confirm for formatting

--- config/nvim/lua/configs/conform.lua
+++ config/nvim/lua/configs/conform.lua
@@ -1,15 +1,43 @@
+local conform = require('conform')
+
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    svelte = { { "prettierd", "prettier" } },
+    javascript = { { "prettierd", "prettier" } },
+    typescript = { { "prettierd", "prettier" } },
+    javascriptreact = { { "prettierd", "prettier" } },
+    typescriptreact = { { "prettierd", "prettier" } },
+    json = { { "prettierd", "prettier" } },
+    graphql = { { "prettierd", "prettier" } },
+    java = { "google-java-format" },
+    kotlin = { "ktlint" },
+    ruby = { "standardrb" },
+    markdown = { { "prettierd", "prettier" } },
+    erb = { "htmlbeautifier" },
+    html = { "htmlbeautifier" },
+    bash = { "beautysh" },
+    proto = { "buf" },
+    rust = { "rustfmt" },
+    yaml = { "yamlfix" },
+    toml = { "taplo" },
+    css = { { "prettierd", "prettier" } },
+    scss = { { "prettierd", "prettier" } },
+    sh = { { "shellcheck" } },
   },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 1000,
+  }
 }
 
-require("conform").setup(options)
+vim.keymap.set({ "n", "v" }, "<leader>l", function()
+  conform.format({
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 1000,
+  })
+end, { desc = "Format file or range (in visual mode)" })
+
+conform.setup(options)
