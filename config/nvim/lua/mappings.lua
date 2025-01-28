b44e158db18dabe9af850c4b83704dffce1d63dd
Porawit Poboonma <ball6847@gmail.com>
2025-01-28 23:06:22 +0700

    fix(nvim): use <D-l> on mac or <C-l> on windows/linux to accept github copilot suggestion

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -11,15 +11,17 @@ map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- copilot accept suggession
--- map("i", "<C-l>", function()
---   vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
--- end, {
---   replace_keycodes = true,
---   nowait = true,
---   silent = true,
---   expr = true,
---   noremap = true,
--- })
+local copilot_accept_key = vim.loop.os_uname().sysname == "Darwin" and "<D-l>" or "<C-l>"
+
+map("i", copilot_accept_key, function()
+  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, {
+  replace_keycodes = true,
+  nowait = true,
+  silent = true,
+  expr = true,
+  noremap = true,
+})
 
 -- keep visual mode after indent
 map("v", "<", "<gv")
