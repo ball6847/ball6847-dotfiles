41db5d51e86e03dede140c8f55f8d07b63537c72
Porawit Poboonma <ball6847@gmail.com>
2025-02-05 11:52:38 +0700

    add command to toggle spellwarn

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -135,22 +135,12 @@ map("n", "<leader>z", function()
   }
 end, { desc = "Toggle zen mode" })
 
--- -- normal mode (easymotion-like)
--- map("n", "<Leader><Leader>w", "<cmd>HopWordAC<CR>", { noremap = true })
--- map("n", "<Leader><Leader>b", "<cmd>HopWordBC<CR>", { noremap = true })
--- map("n", "<Leader><Leader>j", "<cmd>HopLineAC<CR>", { noremap = true })
--- map("n", "<Leader><Leader>k", "<cmd>HopLineBC<CR>", { noremap = true })
---
--- -- visual mode (easymotion-like)
--- map("v", "<Leader><Leader>w", "<cmd>HopWordAC<CR>", { noremap = true })
--- map("v", "<Leader><Leader>b", "<cmd>HopWordBC<CR>", { noremap = true })
--- map("v", "<Leader><Leader>j", "<cmd>HopLineAC<CR>", { noremap = true })
--- map("v", "<Leader><Leader>k", "<cmd>HopLineBC<CR>", { noremap = true })
---
--- -- normal mode (sneak-like)
--- map("n", "s", "<cmd>HopChar2AC<CR>", { noremap = false })
--- map("n", "S", "<cmd>HopChar2BC<CR>", { noremap = false })
---
--- -- visual mode (sneak-like)
--- map("v", "s", "<cmd>HopChar2AC<CR>", { noremap = false })
--- map("v", "S", "<cmd>HopChar2BC<CR>", { noremap = false })
+vim.keymap.set("n", "<leader>ts", function()
+  if vim.opt.spell:get() then
+    require("spellwarn").disable()
+    vim.opt.spell = false
+  else
+    require("spellwarn").enable()
+    vim.opt.spell = true
+  end
+end, { desc = "Toggle SpellWarn" })
