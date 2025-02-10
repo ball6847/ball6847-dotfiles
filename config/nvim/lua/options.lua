d301bc202f6aab8c44e77451ce5e0b4d047c31fe
Porawit Poboonma <ball6847@gmail.com>
2025-02-10 17:45:40 +0700

    add more spell

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -5,7 +5,7 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 20
 vim.opt.clipboard = "unnamedplus"
 vim.opt.exrc = true
-vim.opt.spell = true -- toggle using <leader>ts
+vim.opt.spell = false -- toggle using <leader>ts
 vim.opt.spelllang = "en"
 
 -- local o = vim.o
