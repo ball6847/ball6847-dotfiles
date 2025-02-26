231a814b9c463b0bf0471cc30d49f33f9c358ea0
Porawit Poboonma <ball6847@gmail.com>
2025-02-26 10:58:08 +0700

    fix: remove copilot from cmp config

--- config/nvim/lua/configs/cmp.lua
+++ config/nvim/lua/configs/cmp.lua
@@ -2,27 +2,7 @@
 
 local cmp = require "cmp"
 
-cmp.setup {
-  mapping = {
-    ["<Tab>"] = function(fallback)
-      local copilot = require "copilot.suggestion"
-      if copilot.is_visible() then
-        copilot.accept()
-      elseif cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-  },
-}
+cmp.setup()
 
 cmp.setup.cmdline("/", {
   mapping = cmp.mapping.preset.cmdline(),
