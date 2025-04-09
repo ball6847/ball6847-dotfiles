6528323b3c4adb923d254715c012f514faa8ef23
Porawit Poboonma <ball6847@gmail.com>
2025-04-09 19:01:12 +0700

    update nvim conf

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "onedark",
+  theme = "bearded-arc",
 
   -- hl_override = {
   -- 	Comment = { italic = true },
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -13,12 +13,5 @@ vim.opt.spelllang = "en"
 
 -- configure diagnostics
 vim.diagnostic.config {
-  -- Use the default configuration
-  virtual_lines = true,
-
-  -- Alternatively, customize specific options
-  -- virtual_lines = {
-  --  -- Only show virtual line diagnostics for the current cursor line
-  --  current_line = true,
-  -- },
+  virtual_text = true,
 }
