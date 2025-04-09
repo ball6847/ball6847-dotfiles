5496a21cdde760e70092bb146f68cf0d07a763a5
Porawit Poboonma <ball6847@gmail.com>
2025-04-09 14:01:10 +0700

    add diagnostic to visual line

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -8,6 +8,17 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.exrc = true
 vim.opt.spell = false -- toggle using <leader>ts
 vim.opt.spelllang = "en"
+-- vim.opt.winborder = "rounded" -- this really helps for lsp hover (K) visibility as it add a border to floating windows, but this get conflicted with some plugins especially with telescope
+--
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- configure diagnostics
+vim.diagnostic.config {
+  -- Use the default configuration
+  virtual_lines = true,
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+}
