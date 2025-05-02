84004980b720e39b08b358418b8deb1627a8caef
Porawit Poboonma <ball6847@gmail.com>
2025-05-02 13:22:27 +0700

    bring back scrolloff

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -3,13 +3,12 @@ require "nvchad.options"
 -- add yours here!
 vim.opt.number = true
 vim.opt.relativenumber = true
--- vim.opt.scrolloff = 20
+vim.opt.scrolloff = 15
 vim.opt.clipboard = "unnamedplus"
 vim.opt.exrc = true
 vim.opt.spell = false -- toggle using <leader>ts
 vim.opt.spelllang = "en"
 -- vim.opt.winborder = "rounded" -- this really helps for lsp hover (K) visibility as it add a border to floating windows, but this get conflicted with some plugins especially with telescope
---
 
 -- configure diagnostics
 vim.diagnostic.config {
