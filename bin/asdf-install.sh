609ba62707b1b4afca6aca91844d6a106499e253
Porawit Poboonma <ball6847@gmail.com>
2025-05-06 10:44:35 +0700

    update tools in tool-versions, remove helix and stern from asdf ecosystem

--- bin/asdf-install.sh
+++ bin/asdf-install.sh
@@ -14,7 +14,7 @@ asdf plugin add k3d https://github.com/spencergilbert/asdf-k3d.git
 asdf plugin add deno https://github.com/asdf-community/asdf-deno.git
 asdf plugin add helm https://github.com/Antiarchitect/asdf-helm.git
 asdf plugin add kubecm https://github.com/samhvw8/asdf-kubecm.git
-asdf plugin add helix https://github.com/CSergienko/asdf-helix.git
+# asdf plugin add helix https://github.com/CSergienko/asdf-helix.git
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin add rust
 asdf plugin add neovim
@@ -23,6 +23,6 @@ asdf plugin add nodejs
 asdf plugin add terraform
 asdf plugin add lua-language-server
 asdf plugin add task
-asdf plugin add stern
+# asdf plugin add stern
 
 # TODO: bun
