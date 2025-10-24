3fea4b5776811fa08d7cd63cbbcadb3dba1e4dd6
Porawit Poboonma <ball6847@gmail.com>
2025-10-24 14:35:17 +0700

    style: add go specific indentation style

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -14,3 +14,13 @@ vim.opt.clipboard = "unnamedplus"
 vim.diagnostic.config {
   virtual_text = true,
 }
+
+-- set tab size to 4 for Go files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
