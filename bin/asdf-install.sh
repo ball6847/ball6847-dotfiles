bf5a9d2fe25dd517b46d219f8d847f7dcef721d8
Porawit Poboonma <ball6847@gmail.com>
2025-10-27 16:55:01 +0700

    chore: add redis-cli to tool versions and installation script

--- bin/asdf-install.sh
+++ bin/asdf-install.sh
@@ -27,3 +27,4 @@ asdf plugin add lua-language-server
 asdf plugin add task
 asdf plugin add mockery https://github.com/cabify/asdf-mockery.git
 asdf plugin add golangci-lint https://github.com/hypnoglow/asdf-golangci-lint.git
+asdf plugin add redis-cli
