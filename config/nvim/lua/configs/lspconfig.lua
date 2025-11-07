64d44cff28120fb93f2f49d8c31c7041f3e9a1cb
Porawit Poboonma <ball6847@gmail.com>
2025-11-07 23:03:10 +0700

    update lspconfig to suppress 0.11+ warning

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,37 +1,47 @@
 require "configs.cmp"
 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").nn_init
+local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "lua_ls", "html", "cssls", "bashls", "golangci_lint_ls" }
+-- Configure all LSPs with default settings using new Neovim 0.11+ API
+vim.lsp.config("*", {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+-- Enable LSP servers with default config
+local servers = { "html", "cssls", "bashls", "golangci_lint_ls" }
+for _, server in ipairs(servers) do
+  vim.lsp.enable(server)
 end
 
--- Function to check for the existence of deno.json
--- local function has_deno_json()
---   local current_dir = vim.fn.getcwd()
---   local deno_json = current_dir .. "/deno.json"
---   local deno_json_exists = vim.fn.filereadable(deno_json) == 1
---   return deno_json_exists
--- end
+-- Configure lua_ls with custom settings
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      workspace = {
+        library = {
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+          "${3rd}/luv/library",
+        },
+      },
+    },
+  }
+})
+vim.lsp.enable("lua_ls")
 
 -- deno (typescript)
 -- see https://docs.deno.com/runtime/manual/getting_started/setup_your_environment#vimneovim-via-plugins
 -- note: denols will only enabled if the project root contains deno.json or deno.jsonc
 --       and if your project also contains package.json as well, tsserver will be enabled as well and this will be mess. Make sure you don't have both deno.json and package.json in the same project
 
-lspconfig.denols.setup {
-  on_attach = on_attach,
-  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+vim.lsp.config("denols", {
+  root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc"),
   settings = {
     deno = {
       enable = true,
@@ -39,28 +49,27 @@ lspconfig.denols.setup {
       config = "deno.json",
     },
   },
-}
+})
+vim.lsp.enable("denols")
 
 -- typescript
 -- note: tsserver will be disabled if the project root contains deno.json or deno.jsonc
 
-lspconfig.ts_ls.setup {
+vim.lsp.config("ts_ls", {
+  root_dir = require("lspconfig.util").root_pattern "package.json",
   on_attach = function(client, bufnr)
-    if lspconfig.util.root_pattern("deno.json", "deno.jsonc")(vim.api.nvim_buf_get_name(bufnr)) then
+    if require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")(vim.api.nvim_buf_get_name(bufnr)) then
       client.stop()
     else
       on_attach(client, bufnr)
     end
   end,
-  on_init = on_init,
-  capabilities = capabilities,
-  root_dir = lspconfig.util.root_pattern "package.json",
-  -- single_file_support = false,
-}
+})
+vim.lsp.enable("ts_ls")
 
 -- svelte - configure on_attach with workaround for lsp not picking up ts/js changes.
 -- see - https://github.com/neovim/nvim-lspconfig/issues/725#issuecomment-1837509673
-lspconfig.svelte.setup {
+vim.lsp.config("svelte", {
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePost", {
@@ -71,15 +80,11 @@ lspconfig.svelte.setup {
       end,
     })
   end,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+})
+vim.lsp.enable("svelte")
 
 -- intelephense (php)
-lspconfig.intelephense.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+vim.lsp.config("intelephense", {
   settings = {
     intelephense = {
       -- Add wordpress to the list of stubs
@@ -166,21 +171,18 @@ lspconfig.intelephense.setup {
       },
     },
   },
-}
+})
+vim.lsp.enable("intelephense")
 
 -- protols: detects the root directory by looking for protols.toml
-
-lspconfig.protols.setup {
-  root_dir = lspconfig.util.root_pattern "protols.toml",
-}
+vim.lsp.config("protols", {
+  root_dir = require("lspconfig.util").root_pattern "protols.toml",
+})
+vim.lsp.enable("protols")
 
 -- gopls, setup integration with gofumpt and auto organize imports on save
 -- see: https://github.com/golang/tools/blob/master/gopls/doc/vim.md
-
-lspconfig.gopls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+vim.lsp.config("gopls", {
   settings = {
     gopls = {
       gofumpt = true,
@@ -190,4 +192,5 @@ lspconfig.gopls.setup {
       },
     },
   },
-}
+})
+vim.lsp.enable("gopls")
