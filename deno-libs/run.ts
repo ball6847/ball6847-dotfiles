650ad3844bacf9cb1f06a36c92be3651e10ad656
Porawit Poboonma <ball6847@gmail.com>
2025-11-17 12:25:37 +0700

    chore: remove unused scripts and deno libs

--- deno-libs/run.ts
+++
@@ -1,9 +0,0 @@
-export async function run(bin: string, args: string[]) {
-  const command = new Deno.Command(bin, {
-    args,
-  });
-  const output = await command.output();
-  const decoder = new TextDecoder();
-  const stdout = decoder.decode(output.stdout);
-  return stdout;
-}
