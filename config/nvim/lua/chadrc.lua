e3c6d23c3292500135a14ed1f86b206e6e703a6d
Porawit Poboonma <ball6847@gmail.com>
2025-12-22 00:40:31 +0700

    style(ui): change base46 theme to chadracula

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "tokyodark",
+  theme = "chadracula",
   -- enable true color support for base46 themes
   -- termguicolors = true,  -- This might be handled automatically
 
