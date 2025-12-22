d6ddfa09153edcf51283a404f708e8c437294ab0
Porawit Poboonma <ball6847@gmail.com>
2025-12-22 22:27:14 +0700

    style(ui): change base46 theme to radium

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "chadracula",
+  theme = "radium",
   -- enable true color support for base46 themes
   -- termguicolors = true,  -- This might be handled automatically
 
