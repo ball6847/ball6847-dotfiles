6c67defab7b0f9b759a665caabe043bda430ae08
Porawit Poboonma <ball6847@gmail.com>
2026-01-23 01:33:49 +0700

    style(ui): change base46 theme to onedark

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "aylin",
+  theme = "onedark",
   -- enable true color support for base46 themes
   -- termguicolors = true,  -- This might be handled automatically
 
