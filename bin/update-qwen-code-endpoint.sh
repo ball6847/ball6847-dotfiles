86053c4c22ae8572abc5ed1cfdb5736c7a861f9b
Porawit Poboonma <ball6847@gmail.com>
2026-06-01 07:46:53 +0700

    fix(script): remove empty string from sed command

--- bin/update-qwen-code-endpoint.sh
+++ bin/update-qwen-code-endpoint.sh
@@ -5,6 +5,6 @@ SETTINGS_FILE="$HOME/.qwen/settings.json"
 OLD_URL="https://token-plan.cn-beijing.maas.aliyuncs.com/compatible-mode/v1"
 NEW_URL="https://token-plan.ap-southeast-1.maas.aliyuncs.com/compatible-mode/v1"
 
-sed -i '' "s|${OLD_URL}|${NEW_URL}|g" "$SETTINGS_FILE"
+sed -i "s|${OLD_URL}|${NEW_URL}|g" "$SETTINGS_FILE"
 
 echo "Updated all model endpoints in $SETTINGS_FILE"
